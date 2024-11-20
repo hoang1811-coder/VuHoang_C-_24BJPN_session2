c6f97638ff872cfad136f49226589b7721100ab4
hoang1811-coder <vhoangssz1@gmail.com>
2024-11-21 00:13:13 +0700

    upload

---
+++ ss2.05.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	int dai, rong;
+	scanf("%d %d", &dai, &rong);
+	int chuvi = 2 * (dai + rong);
+	int dientich = dai * rong;
+	printf("%d %d", chuvi, dientich);
+	return 0;
+}
+
