825b073b3a1ea3dc568040846a99f5b34f7f6e95
hoang1811-coder <vhoangssz1@gmail.com>
2024-11-21 00:12:57 +0700

    upload

---
+++ ss2.04.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	// gan gia tri cua canh hinh vuong
+	int canh = 5;
+	// chu vi hinh vuong 
+	int chuvi = canh * 4;
+	prinft("Chu vi cua hinh vuong la: %d \n", chuvi);
+	// dien tich cua hinh vuong
+	int dientich = canh * canh;
+	prinft("Dien tich cua hinh vuong la: %d \n", dientich);
+	return 0;
+}
